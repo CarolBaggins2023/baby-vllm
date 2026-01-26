854a199efd1fe63f7a01b0fa1c4644111264b094
Liu Hongming <67014205+CarolBaggins2023@users.noreply.github.com>
2026-01-26 20:39:18 +0800

    Implement column and row parallel linear layer.

--- src/layers/linear.py
+++ src/layers/linear.py
@@ -38,32 +38,120 @@ class LinearBase(nn.Module):
         else:
             register_parameter('bias', None)
 
-    def weight_loader(self, param: nn.Parameter, loaded_weight: nn.Parameter):
+    def weight_loader(self, param: nn.Parameter, loaded_weights: nn.Parameter):
         """
         Load a saved model checkpoint.
         
         Args:
             param: The parameter to load the weight into.
-            loaded_weight: The weight tensor loaded from the checkpoint.
+            loaded_weights: The weight tensor loaded from the checkpoint.
         
         The core calling sequence of `weight_loader` is as follows:
         for name, param in model.named_parameters():
             if name in checkpoint:
                 # full model parameter
-                loaded_weight = checkpoint[name]
+                loaded_weights = checkpoint[name]
                 
                 # check if the parameter has a custom weight_loader
                 if hasattr(param, 'weight_loader'):
                     # call custom weight_loader
-                    param.weight_loader(param, loaded_weight)
+                    param.weight_loader(param, loaded_weights)
                     # weight_loader will automatically:
                         # 1. extract the shard corresponding to the current GPU
                         # 2. copy it to param.data
                 else:
                     # default: copy directly
-                    param.data.copy_(loaded_weight)
+                    param.data.copy_(loaded_weights)
         """
         raise NotImplementedError("weight_loader must be implemented in subclass.")
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError("forward must be implemented in subclass.")
+    
+class ReplicatedLinear(LinearBase):
+    """
+    The simplest linear layer which does not add more parallelism than the original linear layer.
+    Simply copies the weight from the checkpoint.
+    """
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        bias: bool = True,
+    ):
+        super().__init__(input_size, output_size, bias)
+    
+    def weight_loader(self, param: nn.Parameter, loaded_weights: nn.Parameter):
+        param.data.copy_(loaded_weights)
+    
+    def forward(self, x: torch.Tensor):
+        return nn.functional.linear(x, self.weight, self.bias)
+
+class ColumnParallelLinear(LinearBase):
+    """
+    ColumnParallelLinear is a linear layer where the output dimension is parallelized across multiple devices.
+    Each device only gets a shard of the output features, and the input features are replicated.
+    """
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        bias: bool = True,
+    ):
+        tp_size = dist.get_world_size()
+        # Output dimension must be divisible by the number of devices
+        # to ensure each device gets equal share of the output features.
+        assert output_size%tp_size == 0, f"Output size {output_size} must be divisible by tensor parallel size {tp_size}"
+        super().__init__(input_size, output_size//tp_size, bias, tp_dim=0)
+    
+    def weight_loader(self, param: nn.Parameter, loaded_weights: nn.Parameter):
+        # Calculate the shard size and check if it matches the initialized parameter data size.
+        param_data = param.data
+        full_data_output_size = loaded_weights.size(0)
+        shard_size = full_data_output_size//self.tp_size
+        assert shard_size == param_data.size(0), f"Shard size {shard_size} must be equal to parameter data size {param_data.size(0)}"
+        # Copy the shard corresponding to the current GPU to the parameter data.
+        start_idx = self.tp_rank*shard_size
+        slided_weight = loaded_weights.narrow(0, start_idx, shard_size)
+        param_data.copy_(slided_weight)
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return nn.functional.linear(x, self.weight, self.bias)
+
+class RowParallelLinear(LinearBase):
+    """
+    RowParallelLinear is a linear layer where the input dimension is parallelized across multiple devices.
+    Each device only gets a shard of the input features, and the output features are replicated.
+    """
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        bias: bool = True,
+    ):
+        tp_size = dist.get_world_size()
+        # Input dimension must be divisible by the number of devices
+        # to ensure each device gets equal share of the input features.
+        assert input_size%tp_size == 0, f"Input size {input_size} must be divisible by tensor parallel size {tp_size}"
+        super().__init__(input_size//tp_size, output_size, bias, tp_dim=1)
+    
+    def weight_loader(self, param: nn.Parameter, loaded_weights: nn.Parameter):
+        # Calculate the shard size and check if it matches the initialized parameter data size.
+        param_data = param.data
+        full_data_input_size = loaded_weights.size(1)
+        shard_size = full_data_input_size//self.tp_size
+        assert shard_size == param_data.size(1), f"Shard size {shard_size} must be equal to parameter data size {param_data.size(1)}"
+        # Copy the shard corresponding to the current GPU to the parameter data.
+        start_idx = self.tp_rank*shard_size
+        slided_weight = loaded_weights.narrow(1, start_idx, shard_size)
+        param_data.copy_(slided_weight)
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Row parallel layer usually follows a column parallel layer.
+        # `x` is the output of the column parallel layer.
+        result = nn.functional.linear(x, self.weight, self.bias)
+        # Reduce the output across all devices.
+        if self.tp_size > 1:
+            dist.all_reduce(result, op=dist.ReduceOp.SUM)
+        return result
+    

6f3ff450ad4fec16054ff3df6fdb86e61d739db6
Liu Hongming <67014205+CarolBaggins2023@users.noreply.github.com>
2026-01-30 21:43:49 +0800

    Add init files.

---
+++ src/layers/__init__.py
@@ -0,0 +1,6 @@
+from .activation import SiluAndMul
+from .attention import Attention
+from .embedding_head import VocabParallelEmbedding, ParallelLMHead
+from .layernorm import LayerNorm
+from .linear import *
+from .rotary_embedding import RotaryEmbedding

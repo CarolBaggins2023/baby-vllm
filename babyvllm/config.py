09cae659d7b026f3fe4ab2bdc6ec0936f678ed24
CarolBaggins <carolbaggins2023@gmail.com>
2026-05-05 10:55:25 +0800

    Fix Bug: Remove the check of max number of batched tokens.

--- babyvllm/config.py
+++ babyvllm/config.py
@@ -20,9 +20,10 @@ class Config:
     
     def __post_init__(self):
         assert os.path.isdir(self.model)
+        # Due to the use of the "flash_dattn",
+        # the block size of KV cache must be divisible by 256.
         assert self.kvcache_block_size % 256 == 0
         assert 1 <= self.tensor_parallel_size <= 8
         self.hf_config = AutoConfig.from_pretrained(self.model)
         self.max_model_length = min(self.max_model_length, self.hf_config.max_position_embeddings)
-        assert self.max_num_batched_tokens >= self.max_model_length
     
